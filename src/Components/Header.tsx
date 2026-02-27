5cf3dd9d1dbe69754133bc21656739ce172f5239
shams al labib <samslabib19@gmail.com>
2026-02-27 10:30:58 +0600

    added

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -12,7 +12,6 @@ const Header = () => {
         <Link href={"/login"}>Login</Link>
         <Link href={"/register"}>Register</Link>
         <Link href={"/tutorials"}>Tutorials</Link>
-        <Link href={"/tutorials"}>Tutorials</Link>
       </nav>
     </div>
   );
